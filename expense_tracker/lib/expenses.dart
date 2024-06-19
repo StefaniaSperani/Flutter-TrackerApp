683b2c45bb197aeb27ea3b66a1bc092f8a7f2950
Stefania Sperani <thedarkcry@hotmail.it>
2024-06-19 10:45:23 +0200

    Add appBar and moda

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -12,30 +12,42 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-final List<Expense> _registeredExpenses = [
-  Expense(
-    title: 'Flutter course', 
-    amount: 19.99, 
-    date: DateTime.now(),
-    category: Category.work), 
+  final List<Expense> _registeredExpenses = [
     Expense(
-    title: 'Cinema', 
-    amount: 10.99, 
-    date: DateTime.now(),
-    category: Category.leisure), 
-];
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 10.9954687,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(context: context, 
+    builder: (ctx) => const Text('Modal bottom sheet'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('The chart'),
-          Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
-            ),
-        ]
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
+      body: Column(children: [
+        const Text('The chart'),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        ),
+      ]),
     );
   }
 }
